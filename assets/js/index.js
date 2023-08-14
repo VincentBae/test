1e92e8c936d165197d90c3bf14ca1c4b28ce500f
VincentBae <129641592+VincentBae@users.noreply.github.com>
2023-08-14 18:22:00 +0900

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,9 @@
-$(".slider").slick({
-    infinite: true,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    adaptiveHeight: false,
-    speed: 650,
-    arrows: false,
-    dots: true,
-});
-
-$(document).ready(function() {
-    $(".mail").click(function(e) {
-        alert("문의 시, 반드시 회원 'E-Mail' 또는 '휴대폰 번호'를 기재해주셔야 처리가 가능합니다.");
+(function($){
+    $(document).ready(function(){
+        const swiper = new Swiper('.swiper', {
+            autoplay: true,
+            loop: true,
+            resizeObserver: true,
+        });
     });
-});
+})(jQuery);
